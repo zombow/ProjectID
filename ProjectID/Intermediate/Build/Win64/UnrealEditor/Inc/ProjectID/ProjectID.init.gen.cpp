467ad6c4e81192794dc426c87b25ad6f4a8b21b8
Anyounguk <dksduddnr33@gmail.com>
2022-10-11 17:38:18 +0900

    AYU_1011_part2
    TestCharacter move 컴포넌트화
    카메라 위치 변경

--- ProjectID/Intermediate/Build/Win64/UnrealEditor/Inc/ProjectID/ProjectID.init.gen.cpp
+++ ProjectID/Intermediate/Build/Win64/UnrealEditor/Inc/ProjectID/ProjectID.init.gen.cpp
@@ -23,13 +23,13 @@ void EmptyLinkFunctionForGeneratedCodeProjectID_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x07F35A48,
-				0xB25D8217,
+				0x31282C92,
+				0x7B429C33,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_ProjectID.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_ProjectID.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ProjectID(Z_Construct_UPackage__Script_ProjectID, TEXT("/Script/ProjectID"), Z_Registration_Info_UPackage__Script_ProjectID, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x07F35A48, 0xB25D8217));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ProjectID(Z_Construct_UPackage__Script_ProjectID, TEXT("/Script/ProjectID"), Z_Registration_Info_UPackage__Script_ProjectID, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x31282C92, 0x7B429C33));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- ProjectID/Source/ProjectID/TestCharacter.cpp
+++ ProjectID/Source/ProjectID/TestCharacter.cpp
@@ -3,6 +3,7 @@
 
 #include "TestCharacter.h"
 
+#include "AYU_TestCharacterMoveComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
@@ -13,32 +14,26 @@ ATestCharacter::ATestCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	moveComp = CreateDefaultSubobject<UAYU_TestCharacterMoveComponent>(TEXT("moveComp")); //movecomp 생성 
+
 	myCameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("myCamera")); // 카메라 생성
 	myCameraComp->SetupAttachment(RootComponent); // 카메라를 root에 붙이기
-	myCameraComp->SetRelativeLocation(FVector(-39.56f, 1.75f, 64.f));
+	myCameraComp->SetRelativeLocation(FVector(0.0f, 0.0f, 100.0f));
 	myCameraComp->bUsePawnControlRotation = true;
-
 }
 
 // Called when the game starts or when spawned
 void ATestCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-
-	myMovementComp = this -> GetCharacterMovement(); // 캐릭터 무브먼트 가져오기
-	myMovementComp->MaxWalkSpeed = walk_Speed; // 시작시 이동속도를 walk스피드로 초기화
+	
 }
 
 // Called every frame
 void ATestCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	dir = FTransform(this->GetControlRotation()).TransformVector(dir); //player 이동방향
-	dir.Normalize();
-	dir.Z = 0;
-
-	this->AddMovementInput(dir);
-	dir = FVector::ZeroVector; //미입력시 이동방향 초기화
+	
 }
 
 // Called to bind functionality to input
@@ -46,69 +41,7 @@ void ATestCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAxis(TEXT("Move Forward / Backward"), this, &ATestCharacter::OnAxisMoveForward);
-	PlayerInputComponent->BindAxis(TEXT("Move Right / Left"), this, &ATestCharacter::OnAxisMoveRight);
-
-	PlayerInputComponent->BindAxis(TEXT("Turn Right / Left Mouse"), this, &ATestCharacter::OnAxisLookUp);
-	PlayerInputComponent->BindAxis(TEXT("Look Up / Down Mouse"), this, &ATestCharacter::OnAxisTurn);
-
-	PlayerInputComponent->BindAction(TEXT("Run"), IE_Pressed, this, &ATestCharacter::OnActionRunPressed);
-	PlayerInputComponent->BindAction(TEXT("Run"), IE_Released, this, &ATestCharacter::OnActionRunReleased);
-
-	PlayerInputComponent->BindAction(TEXT("Crouch"), IE_Pressed, this, &ATestCharacter::OnActionCrouchPressed);
-	PlayerInputComponent->BindAction(TEXT("Crouch"), IE_Released, this, &ATestCharacter::OnActionCrouchReleased);
-
-	PlayerInputComponent->BindAction(TEXT("Interact"), IE_Pressed, this, &ATestCharacter::OnActionInteractPressed);
-
-}
-
-void ATestCharacter::OnAxisMoveForward(float value)
-{
-	dir.X = value; //dir.x 값을 value로 대입 (전후)
-}
-
-void ATestCharacter::OnAxisMoveRight(float value)
-{
-	dir.Y = value; //dir.y 값을 value로 대입(좌우)
-}
-
-void ATestCharacter::OnAxisLookUp(float value)
-{
-	this->AddControllerYawInput(value);
-}
-
-void ATestCharacter::OnAxisTurn(float value)
-{
-	this->AddControllerPitchInput(value);
-}
-
-void ATestCharacter::OnActionRunPressed()
-{
-	if (!isCrouch)
-	{
-		myMovementComp->MaxWalkSpeed = run_Speed;
-	}
-}
-
-void ATestCharacter::OnActionRunReleased()
-{
-	myMovementComp->MaxWalkSpeed = walk_Speed;
-}
-
-void ATestCharacter::OnActionCrouchPressed()
-{
-	myMovementComp->MaxWalkSpeed = crouch_Speed;
-	isCrouch = true;
-}
-
-void ATestCharacter::OnActionCrouchReleased()
-{
-	myMovementComp->MaxWalkSpeed = walk_Speed;
-	isCrouch = false;
-}
-
-void ATestCharacter::OnActionInteractPressed()
-{
+	onPlayerInputBindingDelegate.Broadcast(PlayerInputComponent);
 }
 
 
