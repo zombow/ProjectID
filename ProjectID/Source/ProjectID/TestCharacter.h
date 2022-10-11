157df8b302dc46b4f31f91388992f5006920a3a4
Anyounguk <dksduddnr33@gmail.com>
2022-10-11 14:15:02 +0900

    AYU/1011
    testPlayer기본이동구현 (몸은 box)

--- ProjectID/Source/ProjectID/TestCharacter.h
+++ ProjectID/Source/ProjectID/TestCharacter.h
@@ -26,4 +26,36 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	FVector dir; //플레이어 이동 방향 속성
+	bool isCrouch; //플레이어 앉기 상태 속성
+
+	UPROPERTY()
+		class ATestCharacter* me;
+	UPROPERTY()
+		class UCharacterMovementComponent* myMovementComp;
+	UPROPERTY()
+		class UCameraComponent* myCameraComp;
+	UPROPERTY()
+		class USpringArmComponent* mySpringArmComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float crouch_Speed = 300;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float walk_Speed = 500;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float run_Speed = 800;
+
+	void OnAxisMoveForward(float value); //앞 / 뒤 이동
+	void OnAxisMoveRight(float value);  // 오른쪽 / 왼쪽 이동
+	void OnAxisLookUp(float value); //카메라 위아래 회전
+	void OnAxisTurn(float value); //카메라 좌우 회전
+
+	void OnActionRunPressed();
+	void OnActionRunReleased();
+
+	void OnActionCrouchPressed();
+	void OnActionCrouchReleased();
+
+	void OnActionInteractPressed();
+
 };
