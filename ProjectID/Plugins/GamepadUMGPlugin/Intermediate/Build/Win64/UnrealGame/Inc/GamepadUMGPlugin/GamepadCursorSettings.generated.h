a003d2c3a2ecfb4ab325ea3de78d229897cc810b
sungsu8055 <sungsu8055@gmail.com>
2022-11-06 00:54:21 +0900

    메인메뉴, 오프닝 라이트 빌딩

---
+++ ProjectID/Plugins/GamepadUMGPlugin/Intermediate/Build/Win64/UnrealGame/Inc/GamepadUMGPlugin/GamepadCursorSettings.generated.h
@@ -0,0 +1,94 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef GAMEPADUMGPLUGIN_GamepadCursorSettings_generated_h
+#error "GamepadCursorSettings.generated.h already included, missing '#pragma once' in GamepadCursorSettings.h"
+#endif
+#define GAMEPADUMGPLUGIN_GamepadCursorSettings_generated_h
+
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_SPARSE_DATA
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_RPC_WRAPPERS
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUGamepadCursorSettings(); \
+	friend struct Z_Construct_UClass_UGamepadCursorSettings_Statics; \
+public: \
+	DECLARE_CLASS(UGamepadCursorSettings, UDeveloperSettings, COMPILED_IN_FLAGS(0 | CLASS_DefaultConfig | CLASS_Config), CASTCLASS_None, TEXT("/Script/GamepadUMGPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UGamepadCursorSettings) \
+	static const TCHAR* StaticConfigName() {return TEXT("Game");} \
+
+
+
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_INCLASS \
+private: \
+	static void StaticRegisterNativesUGamepadCursorSettings(); \
+	friend struct Z_Construct_UClass_UGamepadCursorSettings_Statics; \
+public: \
+	DECLARE_CLASS(UGamepadCursorSettings, UDeveloperSettings, COMPILED_IN_FLAGS(0 | CLASS_DefaultConfig | CLASS_Config), CASTCLASS_None, TEXT("/Script/GamepadUMGPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UGamepadCursorSettings) \
+	static const TCHAR* StaticConfigName() {return TEXT("Game");} \
+
+
+
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UGamepadCursorSettings(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UGamepadCursorSettings) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UGamepadCursorSettings); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UGamepadCursorSettings); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UGamepadCursorSettings(UGamepadCursorSettings&&); \
+	NO_API UGamepadCursorSettings(const UGamepadCursorSettings&); \
+public:
+
+
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UGamepadCursorSettings(UGamepadCursorSettings&&); \
+	NO_API UGamepadCursorSettings(const UGamepadCursorSettings&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UGamepadCursorSettings); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UGamepadCursorSettings); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UGamepadCursorSettings)
+
+
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_25_PROLOG
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_SPARSE_DATA \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_RPC_WRAPPERS \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_INCLASS \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_SPARSE_DATA \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_INCLASS_NO_PURE_DECLS \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h_28_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> GAMEPADUMGPLUGIN_API UClass* StaticClass<class UGamepadCursorSettings>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_GamepadCursorSettings_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ ProjectID/Plugins/GamepadUMGPlugin/Intermediate/Build/Win64/UnrealGame/Inc/GamepadUMGPlugin/VirtualCursorFunctionLibrary.generated.h
@@ -0,0 +1,105 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+class APlayerController;
+#ifdef GAMEPADUMGPLUGIN_VirtualCursorFunctionLibrary_generated_h
+#error "VirtualCursorFunctionLibrary.generated.h already included, missing '#pragma once' in VirtualCursorFunctionLibrary.h"
+#endif
+#define GAMEPADUMGPLUGIN_VirtualCursorFunctionLibrary_generated_h
+
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_SPARSE_DATA
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execIsCursorOverInteractableWidget); \
+	DECLARE_FUNCTION(execDisableVirtualCursor); \
+	DECLARE_FUNCTION(execEnableVirtualCursor);
+
+
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execIsCursorOverInteractableWidget); \
+	DECLARE_FUNCTION(execDisableVirtualCursor); \
+	DECLARE_FUNCTION(execEnableVirtualCursor);
+
+
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUVirtualCursorFunctionLibrary(); \
+	friend struct Z_Construct_UClass_UVirtualCursorFunctionLibrary_Statics; \
+public: \
+	DECLARE_CLASS(UVirtualCursorFunctionLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/GamepadUMGPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UVirtualCursorFunctionLibrary)
+
+
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_INCLASS \
+private: \
+	static void StaticRegisterNativesUVirtualCursorFunctionLibrary(); \
+	friend struct Z_Construct_UClass_UVirtualCursorFunctionLibrary_Statics; \
+public: \
+	DECLARE_CLASS(UVirtualCursorFunctionLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/GamepadUMGPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UVirtualCursorFunctionLibrary)
+
+
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UVirtualCursorFunctionLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UVirtualCursorFunctionLibrary) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UVirtualCursorFunctionLibrary); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UVirtualCursorFunctionLibrary); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UVirtualCursorFunctionLibrary(UVirtualCursorFunctionLibrary&&); \
+	NO_API UVirtualCursorFunctionLibrary(const UVirtualCursorFunctionLibrary&); \
+public:
+
+
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UVirtualCursorFunctionLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UVirtualCursorFunctionLibrary(UVirtualCursorFunctionLibrary&&); \
+	NO_API UVirtualCursorFunctionLibrary(const UVirtualCursorFunctionLibrary&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UVirtualCursorFunctionLibrary); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UVirtualCursorFunctionLibrary); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UVirtualCursorFunctionLibrary)
+
+
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_14_PROLOG
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_SPARSE_DATA \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_RPC_WRAPPERS \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_INCLASS \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_SPARSE_DATA \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_INCLASS_NO_PURE_DECLS \
+	FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h_17_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> GAMEPADUMGPLUGIN_API UClass* StaticClass<class UVirtualCursorFunctionLibrary>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_ProjectID_Plugins_GamepadUMGPlugin_Source_GamepadUMGPlugin_Public_VirtualCursorFunctionLibrary_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
